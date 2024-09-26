c4ca8cfc7e811ae2d3fae471969dd1e56c024dc9
Levi Yoder Raskin <122696329+Levi-Raskin@users.noreply.github.com>
2024-09-25 17:40:08 -0700

    Update FelsensteinThresholdModel.R

--- FelsensteinThresholdModel.R
+++ FelsensteinThresholdModel.R
@@ -11,11 +11,12 @@ library(aplot)
 # set up ------------------------------------------------------------------
 
 #tree
-tree <- phytools::pbtree(n = 10) #pure birth tree with 10 tips
+tree <- pbtree(n = 5) #pure birth tree with 10 tips
+tree$tip.label <- 1:Ntip(tree)
 plot(tree)
 
 # liabilities
-l1 <- phytools::fastBM(tree, 
+l1 <- fastBM(tree, 
                        sig2 = 1,
                        internal = T) #simulate l1 liabilities on the tree
 ancL1 <- l1[(Ntip(tree) + 1 ):(Ntip(tree) + Nnode(tree))] #subset liabilities at the node
@@ -67,17 +68,16 @@ p3
 
 # inferring ancestral liabilities -----------------------------------------
 
-ng <- 10000 #number of MCMC generations
+ng <- 1000 #number of MCMC generations
 burnIn <- 0.1*ng
 pf <- 10 #print freq
-acceptWiggles <- 0.5 #acceptance/rejection wiggle room
-searchSpread <- 0.05 #SD of Monte Carlo search
+acceptableDiff <- 0.05
 
 propFunc <- function(vec){
   return(sum(vec == 0) / length(vec)) 
 } #proportion of individuals in pop with phenotype 0
 acceptFunc <- function(vec1, vec2){
-  if(all(abs(vec1 - vec2) < acceptWiggles)){
+  if(all(abs(vec1 - vec2) < acceptableDiff)){
     return(TRUE)
   }else{
     return(FALSE)
@@ -85,145 +85,100 @@ acceptFunc <- function(vec1, vec2){
   
 } #acceptance/rejection of proposed new liability
 phenotypeFunc <- function(testL){
-    p1 <- c()
-    for(j in 1:1000){ 
-      liability1 <- rnorm(1, mean = testL, sd = 1)
-      if(liability1 >= t1){
-        p1[j] <- 1  
-      }else{
-        p1[j] <- 0
-      }
+  p1 <- c()
+  for(j in 1:1000){ 
+    liability1 <- rnorm(1, mean = testL, sd = 1)
+    if(liability1 >= t1){
+      p1[j] <- 1  
+    }else{
+      p1[j] <- 0
     }
-    return(p1)
+  }
+  return(p1)
 } #generates phenotypes based on liabilities
-liabProposal <- function(vec){
-  vec2 <- vec
-  vec2[liabsToChange] <- vec2[liabsToChange] + rnorm(length(liabsToChange), 0, searchSpread)
-  
-  return(vec2)
-} #proposes new liability
+
 
 prop0vec1 <- lapply(phenotypeL1, propFunc) #propotion 0 in phenotype L1 (simulated above; our observed data)
 
-liabStart <- phytools::fastBM(tree, 
-                             sig2 = 1,
-                             internal = T) #starting liabilities
-liabRes <- list()
+startLiabilities <- fastBM(tree,
+                           internal = T) #starting liabilities
 
-
+resList <- list()
 for(i in 1:ng){
-  if(i == 1){
-    liabsToChange <- 1:(Ntip(tree) + Nnode(tree))
-  }
-  
-  j <- 1
+  j <- 1 #counter var
   repeat{
-    j <- j+1
-    if(j > 10000 && i < 100){ 
-      testLiab <- phytools::fastBM(tree, 
-                                  sig2 = 1,
-                                  internal = T)
-      testPhenotype <- lapply(testLiab[1:Ntip(tree)], phenotypeFunc)
-      print("random search")
-    }else{
-      testLiab <- liabProposal(liabStart,liabsToChange)
-      testPhenotype <- lapply(testLiab[1:Ntip(tree)], phenotypeFunc)
-      if(searchSpread < 2){
-        searchSpread <- searchSpread*1.0001
+    testPhenotypes <- lapply(lapply(startLiabilities, phenotypeFunc), propFunc) #test phenotypes based on the test liabilities
+    if(acceptFunc(unlist(testPhenotypes[1:Ntip(tree)]), unlist(prop0vec1))){
+      #choose node to update:
+      p <- sample(1 : (Nnode(tree) + Ntip(tree)), 1) #sample one of the tree's nodes
+      pAnc <- tree$edge[which(tree$edge[,2] == p), 1] #get the singular ancestor of that node
+      pDesc <- tree$edge[which(tree$edge[,1] == p), 2] #get the descendants of that node
+      if(length(pDesc) > 1 &&  length(pAnc) == 1){ #if p is an internal node that is not the root
+        pAncVal <- startLiabilities[which(names(startLiabilities) == pAnc)] #ancestor value
+        pDescVal1 <- startLiabilities[which(names(startLiabilities) == pDesc[1])] #descendant 1 value
+        pDescVal2 <- startLiabilities[which(names(startLiabilities) == pDesc[2])] #descendant 2 value
+        mean <- ((1/castor::get_pairwise_distances(tree, p, pAnc)) * pAncVal + 
+                   (1/castor::get_pairwise_distances(tree, p, pDesc[1])) * pDescVal1 + 
+                   (1/castor::get_pairwise_distances(tree, p, pDesc[1])) * pDescVal2
+        ) / ((1/castor::get_pairwise_distances(tree, p, pAnc)) + 
+               (1/castor::get_pairwise_distances(tree, p, pDesc[1])) + 
+               (1/castor::get_pairwise_distances(tree, p, pDesc[1])))
+        variance <- 1/ ((1/castor::get_pairwise_distances(tree, p, pAnc)) + 
+                          (1/castor::get_pairwise_distances(tree, p, pDesc[1])) + 
+                          (1/castor::get_pairwise_distances(tree, p, pDesc[1])))
+        startLiabilities[which(names(startLiabilities) == p)] <- rnorm(1, mean, sqrt(variance))
+      }else if(length(pAnc) == 1 && length(pDesc) == 0){
+        #tip; mean = pAnc val; var = sqrt branch length
+        pAncVal <- startLiabilities[which(names(startLiabilities) == pAnc)]
+        startLiabilities[which(names(startLiabilities) == p)] <- rnorm(1, pAncVal, sqrt(castor::get_pairwise_distances(tree, p, pAnc)))
+      }else if(length(pAnc) == 0 && length(pDesc) > 0){
+        pDescVal1 <- startLiabilities[which(names(startLiabilities) == pDesc[1])]
+        pDescVal2 <- startLiabilities[which(names(startLiabilities) == pDesc[2])]
+        mean <- ( (1/castor::get_pairwise_distances(tree, p, pDesc[1])) * pDescVal1 + 
+                   (1/castor::get_pairwise_distances(tree, p, pDesc[1])) * pDescVal2
+        ) / (  (1/castor::get_pairwise_distances(tree, p, pDesc[1])) + 
+               (1/castor::get_pairwise_distances(tree, p, pDesc[1])))
+        variance <- 1/ ( (1/castor::get_pairwise_distances(tree, p, pDesc[1])) + 
+                          (1/castor::get_pairwise_distances(tree, p, pDesc[1])))
+        startLiabilities[which(names(startLiabilities) == p)] <- rnorm(1, mean, sqrt(variance))
       }
-    }
-   
-    if(acceptFunc(unlist(lapply(testPhenotype, propFunc)), unlist(prop0vec1)) == TRUE){
-      liabStart <- testLiab
-      liabRes[[i]] <- testLiab
-    
-      if(i %% pf == 0){
-        print(paste("NG: ", i))
-      }
-      if(acceptWiggles > 0.05){
-        acceptWiggles <- acceptWiggles*0.99
-      }
-      searchSpread <- 0.05
+      resList[[i]] <- startLiabilities
       break
     }else{
-      liabsToChange <- c()
-      for(k in 1:length(testPhenotype)){
-        if(abs(lapply(testPhenotype, propFunc)[[k]] - prop0vec1[[k]]) >= acceptWiggles){
-          liabsToChange <- c(liabsToChange, k)
+      
+      if(i == 1 || j > 100000){
+        startLiabilities <- fastBM(tree,
+                                   internal = T)
+      }else{
+        p <- sample(1 : Ntip(tree), 1)
+        pAnc <- tree$edge[which(tree$edge[,2] == p), 1]
+        pAncVal <- startLiabilities[which(names(startLiabilities) == pAnc)]
+        testLiab <- startLiabilities
+        testLiab[which(names(startLiabilities) == p)] <- rnorm(1, pAncVal, sqrt(castor::get_pairwise_distances(tree, p, pAnc)))
+        
+        testPhenotypesInt <- lapply(lapply(testLiab, phenotypeFunc), propFunc) #test phenotypes based on the test liabilities
+    
+        if(acceptFunc(unlist(testPhenotypesInt[1:Ntip(tree)]), unlist(prop0vec1))){
+          startLiabilities <- testLiab
         }
       }
-    }
-  }
-  
-}
-
-dat <- c()
-for(i in burnIn:ng){
-  dat[i] <- liabRes[[i]][11]  
-}
-hist(dat) 
-l1[11]
-
-
-
-
-
-
-
-
-liabProposal <- function(vec){
-  vec2 <- vec + phytools::fastBM(tree, 
-                           sig2 = 0.05,
-                           internal = T)
-  return(vec2)
-} #proposes new liability
-
-prop0vec1 <- lapply(phenotypeL1, propFunc) #propotion 0 in phenotype L1 (simulated above; our observed data)
-
-liabStart <- phytools::fastBM(tree, 
-                              sig2 = 1,
-                              internal = T) #starting liabilities
-liabRes <- list()
-
-
-for(i in 1:ng){
-  j <- 1
-  repeat{
-    j <- j+1
-    if(j > 10000 && i < 100){ 
-      testLiab <- phytools::fastBM(tree, 
-                                   sig2 = 1,
-                                   internal = T)
-      testPhenotype <- lapply(testLiab[1:Ntip(tree)], phenotypeFunc)
-      print("random search")
-    }else{
-      testLiab <- liabProposal(liabStart)
-      testPhenotype <- lapply(testLiab[1:Ntip(tree)], phenotypeFunc)
-      if(searchSpread < 2){
-        searchSpread <- searchSpread*1.0001
-      }
-    }
-    
-    if(acceptFunc(unlist(lapply(testPhenotype, propFunc)), unlist(prop0vec1)) == TRUE){
-      liabStart <- testLiab
-      liabRes[[i]] <- testLiab
       
-      if(i %% pf == 0){
-        print(paste("NG: ", i))
+      
+      if(j %% 100 == 0){
+        print(paste("NG: ", i, "on rep ", j, " with no suitable liability found"))
       }
-      if(acceptWiggles > 0.05){
-        acceptWiggles <- acceptWiggles*0.99
-      }
-      searchSpread <- 0.05
-      break
     }
+    j <- j+1
   }
   
+  if(i %% pf == 0){
+    print(paste("NG: ", i))
+  }
 }
 
 dat <- c()
 for(i in burnIn:ng){
-  dat[i] <- liabRes[[i]][11]  
+  dat<- c(dat, resList[[i]][11])
 }
 hist(dat) 
 l1[11]
